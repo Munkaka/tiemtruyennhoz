1c154edad6f0acdd1c37927bc4baf5e31d3257fd
Meouteam <116555575+Munkaka@users.noreply.github.com>
2025-12-17 11:52:39 +0700

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://tiemtruyennhoz.muncute12121231-503.workers.dev';
+const BASE_URL = 'https://tiemtruyennhoz.muncute12121231-503.workers.dev';
 
 export const api = {
   async get(endpoint: string) {
