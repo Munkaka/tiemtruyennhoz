16c6aa076be26a355977f74ea8ecf1366c930c5a
Meouteam <116555575+Munkaka@users.noreply.github.com>
2025-12-20 09:01:06 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './index.css';
+import './index.css'; // Dòng này cực kỳ quan trọng để nhận diện giao diện
 
 const container = document.getElementById('root');
 if (container) {
